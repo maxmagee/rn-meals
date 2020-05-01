e31a598f7a690e7c20bbeca3d967ceeb6c3d0a4f
Max Magee <max.r.magee@gmail.com>
2020-04-30 21:02:46 -0500

    Added basic stack navigation proof of concept.

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,16 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 const CategoriesScreen = (props) => {
+  const { navigation } = props;
+
+  const pressHandler = () => {
+    navigation.navigate({ routeName: "CategoryMeals" });
+  };
+
   return (
     <View style={styles.screen}>
       <Text>Categories Screen</Text>
+      <Button title="Go To Meals!" onPress={pressHandler} />
     </View>
   );
 };
 
-CategoriesScreen.propTypes = {};
+CategoriesScreen.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
 CategoriesScreen.defaultProps = {};
 
 const styles = StyleSheet.create({
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,16 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 const CategoryMealsScreen = (props) => {
+  const { navigation } = props;
+
+  const pressHandler = () => {
+    navigation.navigate({ routeName: "MealDetail" });
+  };
+
   return (
     <View style={styles.screen}>
       <Text>Category Meals Screen</Text>
+      <Button title="Go to Meals Detail" onPress={pressHandler} />
     </View>
   );
 };
 
-CategoryMealsScreen.propTypes = {};
+CategoryMealsScreen.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
 CategoryMealsScreen.defaultProps = {};
 
 const styles = StyleSheet.create({

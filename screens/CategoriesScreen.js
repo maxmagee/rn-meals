9fd52d74bed1e76f98ed2167b4a393dcb9272f0f
Max Magee <max.r.magee@gmail.com>
2020-05-02 12:47:27 -0500

    Customized the CategoriesScreen header.

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,22 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Button, FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
+import colors from "../constants/colors";
 import dummyData from "../data/dummy-data";
 
-const renderGridItem = (itemData) => {
-  return (
-    <View style={styles.gridItem}>
-      <Text>{itemData.item.title}</Text>
-    </View>
-  );
-};
-
 const CategoriesScreen = (props) => {
+  const { navigation } = props;
+
+  const itemPressHandler = () => {
+    navigation.navigate({ routeName: "CategoryMeals" });
+  };
+
+  const renderGridItem = (itemData) => {
+    return (
+      <TouchableOpacity style={styles.gridItem} onPress={itemPressHandler}>
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return <FlatList data={dummyData.categories} renderItem={renderGridItem} numColumns={2} />;
 };
 
-CategoriesScreen.propTypes = {};
+CategoriesScreen.navigationOptions = {
+  headerStyle: {
+    backgroundColor: colors.primary,
+  },
+  headerTintColor: "white",
+  headerTitle: "Meal Categories",
+};
+
+CategoriesScreen.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 CategoriesScreen.defaultProps = {};
 

639ee2f4e40168d79efc958dc30a57cfc489e6d5
Max Magee <max.r.magee@gmail.com>
2020-05-04 13:01:57 -0500

    Set up the tab navigation.

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,8 +1,13 @@
+import React from "react";
 import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons } from '@expo/vector-icons'; //eslint-disable-line
+
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
+import FavoritesScreen from "../screens/FavoritesScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
 
 import colors from "../constants/colors";
@@ -28,4 +33,31 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(MealsNavigator);
+const MealFavTabNavigator = createBottomTabNavigator(
+  {
+    Favorites: {
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
+        },
+      },
+      screen: FavoritesScreen,
+    },
+    Meals: {
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />;
+        },
+      },
+      screen: MealsNavigator,
+    },
+  },
+  {
+    order: ["Meals", "Favorites"],
+    tabBarOptions: {
+      activeTintColor: colors.secondary,
+    },
+  }
+);
+
+export default createAppContainer(MealFavTabNavigator);
